29b12501adea525c8600541d34952df36bb406bb
rkaminski <remigiusz20kaminski@gmail.com>
2026-08-18 16:48:54 +0200

    reverting url change

--- constants/http.ts
+++ constants/http.ts
@@ -1,12 +1,10 @@
-// export const BASE_URL: string =
-//   process.env.EXPO_PUBLIC_API_BASE_URL ??
-//   "https://ca-pocket-type-chart-dev.thankfulbush-d0597784.polandcentral.azurecontainerapps.io/api";
-
-import { Platform } from "react-native";
+export const BASE_URL: string =
+  process.env.EXPO_PUBLIC_API_BASE_URL ??
+  "https://ca-pocket-type-chart-dev.thankfulbush-d0597784.polandcentral.azurecontainerapps.io/api";
 
 //export const BASE_URL: string = "http://192.168.100.4:5062/api";
-export const BASE_URL: string =
-  Platform.OS === "web" ? "http://localhost:5062/api" : "http://10.0.2.2:5062/api";
+//export const BASE_URL: string =
+//Platform.OS === "web" ? "http://localhost:5062/api" : "http://10.0.2.2:5062/api";
 //export const BASE_URL : string = "https://pockettypechart-fjahb0dzb3ebh5dk.polandcentral-01.azurewebsites.net/api"
 export const POKETYPES_ENDPOINT: string = "/poketypes";
 export const DAMAGERELATIONS_ENDPOINT: string = "/damagerelations";
